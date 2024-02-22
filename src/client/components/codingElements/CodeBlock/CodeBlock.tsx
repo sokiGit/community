b6091d2bdacc90b4e22cca5a222f31b1e245cdd6
sokiGit <soki.personal@gmail.com>
2024-02-22 16:33:27 +0100

    eslint disable no-explicity-any

--- src/client/components/codingElements/CodeBlock/CodeBlock.tsx
+++ src/client/components/codingElements/CodeBlock/CodeBlock.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import "./CodeBlock.css"
 
-export default function CodeBlock(props) {
+export default function CodeBlock(props:any) {
     return (
         <code className={"CodeBlock "+(props.stretch ? "CodeBlockStretch" : "")}>
             {props.children}
--- src/client/components/global/Main/Main.tsx
+++ src/client/components/global/Main/Main.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import "./Main.css"
 
-export default function Main(props) {
+export default function Main(props:any) {
     return (
         <main id={"MainContent"} className={props.className}>
             {props.children}
